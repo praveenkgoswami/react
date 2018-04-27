74a29fb46ba2dbaffe0112f6e7e0ddf4815debac
Praveen-Kumar <praveen.kumar@tothenew.com>
2018-04-28 01:16:22 +0530

    localhost set to 3000

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.join(__dirname, 'src'),
-        port: 3002,
+        port: 3000,
         publicPath: '/'
     },
     module: {
