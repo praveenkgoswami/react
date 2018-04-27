4d951d947537e46091c49fafcb74cfb3b932e61e
praveenkgoswami <praveenkumargoswami@live.com>
2018-04-28 00:41:37 +0530

    cart app code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# React-training# react
 # react
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        port: 3002,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            }
+        ]
+    },
+};
